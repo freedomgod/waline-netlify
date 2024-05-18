8841dd4455c3b5439c009bd313a76547394d965b
freedomgod <59652488+freedomgod@users.noreply.github.com>
2024-05-18 13:41:21 +0800

    Update comment.js 撤销emoji的设置

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -5,11 +5,6 @@ const Waline = require('@waline/vercel');
 const serverless = require('serverless-http');
 
 const app = Waline({
-  emoji: [
-    'https://cdn.jsdelivr.net/gh/walinejs/emojis@1.0.0/weibo',
-    'https://cdn.jsdelivr.net/gh/walinejs/emojis@1.0.0/bilibili',
-    'https://cdn.jsdelivr.net/gh/walinejs/emojis@1.1.0/alus',
-  ],
   async postSave(comment) {
     // do what ever you want after save comment
   },
