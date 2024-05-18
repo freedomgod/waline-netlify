5a407a9628d60e3d799cd3fdb9702b038241234e
freedomgod <59652488+freedomgod@users.noreply.github.com>
2024-05-18 13:11:13 +0800

    Update comment.js test add emoji

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -5,6 +5,10 @@ const Waline = require('@waline/vercel');
 const serverless = require('serverless-http');
 
 const app = Waline({
+  emoji: [
+    'https://cdn.jsdelivr.net/gh/walinejs/emojis@1.0.0/weibo',
+    'https://cdn.jsdelivr.net/gh/walinejs/emojis@1.0.0/bilibili',
+  ],
   async postSave(comment) {
     // do what ever you want after save comment
   },
