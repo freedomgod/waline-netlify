732b3c35c932a8ccf8575dc0cfab5039453ef9a4
freedomgod <59652488+freedomgod@users.noreply.github.com>
2024-05-18 13:14:37 +0800

    Update comment.js update emoji content

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -8,6 +8,7 @@ const app = Waline({
   emoji: [
     'https://cdn.jsdelivr.net/gh/walinejs/emojis@1.0.0/weibo',
     'https://cdn.jsdelivr.net/gh/walinejs/emojis@1.0.0/bilibili',
+    'https://cdn.jsdelivr.net/gh/walinejs/emojis@1.1.0/alus',
   ],
   async postSave(comment) {
     // do what ever you want after save comment
